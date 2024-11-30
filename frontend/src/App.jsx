d34f042ee167661fd3180993b4d8ee6d52ecdbe0
devilghostg <mister.devil6@ggmail.com>
2024-11-30 19:59:57 +0400

    add page auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AudioProvider } from './context/AudioContext';
 import Home from './components/pages/Home';
-import Options from './components/pages/Options';
 import CharacterCreation from './components/pages/CharacterCreation';
+import Options from './components/pages/Options';
+import Auth from './components/pages/Auth';
 import './App.css';
 
 function App() {
@@ -10,9 +11,11 @@ function App() {
     <AudioProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/options" element={<Options />} />
+          <Route path="/" element={<Navigate to="/auth" />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/character-creation" element={<CharacterCreation />} />
+          <Route path="/options" element={<Options />} />
         </Routes>
       </Router>
     </AudioProvider>
