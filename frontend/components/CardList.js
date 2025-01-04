79ed19f80e811f34b3ee877069f47a8261414635
Kevin Lamotte <84518512+Kevin-Lamotte974@users.noreply.github.com>
2025-01-04 11:07:22 +0100

    Réctification css global

--- frontend/components/CardList.js
+++ frontend/components/CardList.js
@@ -80,119 +80,121 @@ function CardList() {
     if (error) return <div>Error: {error}</div>;
 
     return (
-        <div className="container mx-auto py-8">
-            <div className="flex justify-between mb-4">
-                <div>
-                    <label htmlFor="rarity-filter" className="mr-2 text-white">Filter by Rarity:</label>
-                    <select id="rarity-filter" value={filterRarity} onChange={handleFilterRarity} className="mr-4">
-                        <option value="">All</option>
-                        <option value="commune">Commune</option>
-                        <option value="rare">Rare</option>
-                        <option value="épique">Épique</option>
-                        <option value="légendaire">Légendaire</option>
-                    </select>
+        <div class="cardlist">
+            <div className="container mx-auto py-8">
+                <div className="flex justify-between mb-4">
+                    <div>
+                        <label htmlFor="rarity-filter" className="mr-2 text-white">Filter by Rarity:</label>
+                        <select id="rarity-filter" value={filterRarity} onChange={handleFilterRarity} className="mr-4">
+                            <option value="">All</option>
+                            <option value="commune">Commune</option>
+                            <option value="rare">Rare</option>
+                            <option value="épique">Épique</option>
+                            <option value="légendaire">Légendaire</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="class-filter" className="mr-2 text-white">Filter by Class:</label>
+                        <select id="class-filter" value={filterClass} onChange={handleFilterClass} className="mr-4">
+                            <option value="">All</option>
+                            <option value="Éclaireur des Vents">Éclaireur des Vents</option>
+                            <option value="Cristallomancien">Cristallomancien</option>
+                            <option value="Négociant des Cieux">Négociant des Cieux</option>
+                            <option value="Archéologue des Ruines">Archéologue des Ruines</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="sort-option" className="mr-2 text-white">Sort by:</label>
+                        <select id="sort-option" value={sortOption} onChange={handleSortOption}>
+                            <option value="">None</option>
+                            <option value="rarity">Rarity</option>
+                            <option value="class">Class</option>
+                        </select>
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor="class-filter" className="mr-2 text-white">Filter by Class:</label>
-                    <select id="class-filter" value={filterClass} onChange={handleFilterClass} className="mr-4">
-                        <option value="">All</option>
-                        <option value="Éclaireur des Vents">Éclaireur des Vents</option>
-                        <option value="Cristallomancien">Cristallomancien</option>
-                        <option value="Négociant des Cieux">Négociant des Cieux</option>
-                        <option value="Archéologue des Ruines">Archéologue des Ruines</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="sort-option" className="mr-2 text-white">Sort by:</label>
-                    <select id="sort-option" value={sortOption} onChange={handleSortOption}>
-                        <option value="">None</option>
-                        <option value="rarity">Rarity</option>
-                        <option value="class">Class</option>
-                    </select>
-                </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                {filteredCards.map((card) => (
-                    <div
-                        key={card.id}
-                        className={`card ${getCardRarity(card.rarity)} cursor-pointer hover:scale-105 transition-transform duration-200`}
-                        onClick={() => setSelectedCard(card)}
-                    >
-                        <div className="card-frame">
-                            <div className="rare-emblem">
-                                <div className="emblem-frame">
-                                    <Image
-                                        src={`/assets/images/classes/${card.class_association?.toLowerCase().replace(/ /g, '_').normalize('NFD').replace(/[\u0300-\u036f]/g, '')}_logo.svg`}
-                                        alt={`emblème ${card.class_association}`}
-                                        className="emblem-image"
-                                        width={50}
-                                        height={50}
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="energy-notch">
-                                <Image src="/assets/images/crystal.png" alt="crystal" class="energy-crystal" width={50} height={50}/>
-                                <span class="energy-value">{card.energy_cost}</span>
-                            </div>
-
-                            {card.damage_base != 0 && (
-                                <div class={`damage-container ${card.damage_base < 0 ? "heal" : "attack"}`}>
-                                    {card.damage_base < 0 ? <GiHealthNormal color='green' /> : <GiBroadsword />}
-                                    <span class="damage-value">{Math.abs(card.damage_base)}</span>
-                                </div>
-                            )}
-
-                            <div className="card-stats">
-                                {card.stats.force > 0 && (
-                                    <span className="stat" title="force">
-                                        {card.stats.force}<FaFistRaised className="inline ml-1" />
-                                    </span>
-                                )}
-                                {card.stats.agilite > 0 && (
-                                    <span className="stat" title="agilite">
-                                        {card.stats.agilite}<GiLeatherBoot className="inline ml-1" />
-                                    </span>
-                                )}
-                                {card.stats.intelligence > 0 && (
-                                    <span className="stat" title="intelligence">
-                                        {card.stats.intelligence}<GiWhiteBook className="inline ml-1" />
-                                    </span>
-                                )}
-                                {card.stats.charisme > 0 && (
-                                    <span className="stat" title="charisme">
-                                        {card.stats.charisme}<GiMoai className="inline ml-1" />
-                                    </span>
-                                )}
-                            </div>
-
-                            <div className="card-name">{card.name}</div>
-
-                            <div className="inner-frame">
-                                <div className="crystal-container">
-                                    <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-tl" />
-                                    <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-tr" />
-                                    <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-bl" />
-                                    <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-br" />
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                    {filteredCards.map((card) => (
+                        <div
+                            key={card.id}
+                            className={`card ${getCardRarity(card.rarity)} cursor-pointer hover:scale-105 transition-transform duration-200`}
+                            onClick={() => setSelectedCard(card)}
+                        >
+                            <div className="card-frame">
+                                <div className="rare-emblem">
+                                    <div className="emblem-frame">
+                                        <Image
+                                            src={`/assets/images/classes/${card.class_association?.toLowerCase().replace(/ /g, '_').normalize('NFD').replace(/[\u0300-\u036f]/g, '')}_logo.svg`}
+                                            alt={`emblème ${card.class_association}`}
+                                            className="emblem-image"
+                                            width={50}
+                                            height={50}
+                                        />
+                                    </div>
                                 </div>
 
-                                <div className="card-image-container">
-                                    <Image
-                                        src={card.image}
-                                        alt={card.name}
-                                        fill
-                                        style={{ objectFit: 'cover' }}
-                                        priority
-                                    />
+                                <div class="energy-notch">
+                                    <Image src="/assets/images/crystal.png" alt="crystal" class="energy-crystal" width={50} height={50} />
+                                    <span class="energy-value">{card.energy_cost}</span>
                                 </div>
 
-                                <div className="special-effect">
-                                    {card.special_effect}
+                                {card.damage_base != 0 && (
+                                    <div class={`damage-container ${card.damage_base < 0 ? "heal" : "attack"}`}>
+                                        {card.damage_base < 0 ? <GiHealthNormal color='green' /> : <GiBroadsword />}
+                                        <span class="damage-value">{Math.abs(card.damage_base)}</span>
+                                    </div>
+                                )}
+
+                                <div className="card-stats">
+                                    {card.stats.force > 0 && (
+                                        <span className="stat" title="force">
+                                            {card.stats.force}<FaFistRaised className="inline ml-1" />
+                                        </span>
+                                    )}
+                                    {card.stats.agilite > 0 && (
+                                        <span className="stat" title="agilite">
+                                            {card.stats.agilite}<GiLeatherBoot className="inline ml-1" />
+                                        </span>
+                                    )}
+                                    {card.stats.intelligence > 0 && (
+                                        <span className="stat" title="intelligence">
+                                            {card.stats.intelligence}<GiWhiteBook className="inline ml-1" />
+                                        </span>
+                                    )}
+                                    {card.stats.charisme > 0 && (
+                                        <span className="stat" title="charisme">
+                                            {card.stats.charisme}<GiMoai className="inline ml-1" />
+                                        </span>
+                                    )}
+                                </div>
+
+                                <div className="card-name">{card.name}</div>
+
+                                <div className="inner-frame">
+                                    <div className="crystal-container">
+                                        <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-tl" />
+                                        <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-tr" />
+                                        <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-bl" />
+                                        <img src="/assets/images/crystal.png" alt="crystal" class="crystal crystal-br" />
+                                    </div>
+
+                                    <div className="card-image-container">
+                                        <Image
+                                            src={card.image}
+                                            alt={card.name}
+                                            fill
+                                            style={{ objectFit: 'cover' }}
+                                            priority
+                                        />
+                                    </div>
+
+                                    <div className="special-effect">
+                                        {card.special_effect}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
 
             {selectedCard && (
